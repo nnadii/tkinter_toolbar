b916d764eab39301b74872b32486aa6bf5ea5c53
Chimennadi <nnadichime04@gmail.com>
2024-05-24 08:54:22 +0100

    new update

--- message.py
+++ message.py
@@ -4,7 +4,7 @@ import tkinter.messagebox
 root = Tk()
 
 tkinter.messagebox.showinfo("Title", "This is awesome")
-response = tkinter.messagebox.askquestion("Question", "Do you like coffee")
+response = tkinter.messagebox.askquestion("Question", "Do you like coffee?")
 
 if response == "yes":
     print("Here is a coffee for you")
